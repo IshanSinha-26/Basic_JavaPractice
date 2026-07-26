2eca233d5a353b0a4b9970bf3f65eee6f6b29f1b
IshanSinha-26 <ishansinha2611@gmail.com>
2026-07-26 12:28:42 +0530

    11thquestion

---
+++ Array/arraySorted.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class arraySorted{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean sorted=true;
+        for(int i=0; i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                sorted=false;
+                break;
+            }
+        }
+        if(sorted==true){
+            System.out.println("Array is sorted in ascending order.");
+        }
+        else{
+            System.out.println("Array is not sorted in ascending order.");
+        }
+    }
+}
