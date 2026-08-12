a3a3e7df32001e1434e1af06fa177f4d74dbc244
IshanSinha-26 <ishansinha2611@gmail.com>
2026-08-12 15:35:45 +0530

    str4

---
+++ String/palindromeCheck.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class palindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); 
+
+        System.out.print("enter the string: ");
+        String str=sc.nextLine();
+
+        int left=0;
+        int right=str.length()-1;
+        boolean palindrome=true;
+
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                palindrome=false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if(palindrome==true){
+            System.out.print("this is palindrome");
+        }else{
+            System.out.print("this is not palindrome");
+        }
+        sc.close();
+    }
+}
