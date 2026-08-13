983b072f246f4a5b68df8fcfa1a7638ba0e65afa
IshanSinha-26 <ishansinha2611@gmail.com>
2026-08-13 16:51:54 +0530

    update

--- String/countWords.java
+++ String/countWords.java
@@ -1,23 +1,50 @@
-import java.util.*;
-public class countWords {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("enter the sentence: ");
-        String str=sc.nextLine();
+// import java.util.*;
+// public class countWords {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("enter the sentence: ");
+//         String str=sc.nextLine();
 
-        int count=1;
-        str=str.trim();
+//         int count=1;
+//         str=str.trim(); 
         
-        if(str.isEmpty()){
-            count=0;
+//         if(str.isEmpty()){
+//             count=0;
+//         }
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i)==' '){
+//                 count++;
+//             }
+//         }
+//         System.out.print("word count: "+count);
+//         sc.close();
+//     }
+    
+// }
+
+import java.util.*;
+
+public class countWords {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the sentence: ");
+        String str = sc.nextLine();
+
+        str = str.trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Word count: 0");
         }
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)==' '){
-                count++;
-            }
+        else {
+
+            String[] words = str.split("\\s+");
+
+            System.out.println("Word count: " + words.length);
         }
-        System.out.print("word count: "+count);
+
         sc.close();
     }
-    
 }
--- String/palindromeCheck.java
+++ String/palindromeCheck.java
@@ -1,32 +1,3 @@
-// import java.util.*;
-// public class palindromeCheck {
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in); 
-
-//         System.out.print("enter the string: ");
-//         String str=sc.nextLine();
-
-//         int left=0;
-//         int right=str.length()-1;
-//         boolean palindrome=true;
-
-//         while(left<right){
-//             if(str.charAt(left)!=str.charAt(right)){
-//                 palindrome=false;
-//                 break;
-//             }
-//             left++;
-//             right--;
-//         }
-//         if(palindrome==true){
-//             System.out.print("this is palindrome");
-//         }else{
-//             System.out.print("this is not palindrome");
-//         }
-//         sc.close();
-//     }
-// }
-
 import java.util.*;
 public class palindromeCheck {
     public static void main(String[] args) {
@@ -35,15 +6,44 @@ public class palindromeCheck {
         System.out.print("enter the string: ");
         String str=sc.nextLine();
 
-        String reverse="";
-        for(int i=str.length()-1; i>=0; i--){
-            reverse=reverse + str.charAt(i);
+        int left=0;
+        int right=str.length()-1;
+        boolean palindrome=true;
+
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                palindrome=false;
+                break;
+            }
+            left++;
+            right--;
         }
-        if(str.equals(reverse)){
-            System.out.print("it is palindrome");
+        if(palindrome==true){
+            System.out.print("this is palindrome");
         }else{
-            System.out.print("it is not");
+            System.out.print("this is not palindrome");
         }
         sc.close();
     }
 }
+
+// import java.util.*;
+// public class palindromeCheck {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in); 
+
+//         System.out.print("enter the string: ");
+//         String str=sc.nextLine();
+
+//         String reverse="";
+//         for(int i=str.length()-1; i>=0; i--){
+//             reverse=reverse + str.charAt(i);
+//         }
+//         if(str.equals(reverse)){
+//             System.out.print("it is palindrome");
+//         }else{
+//             System.out.print("it is not");
+//         }
+//         sc.close();
+//     }
+// }
