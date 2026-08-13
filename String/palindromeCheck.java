49f7c170b47a7f82c752c2dfbd3b28286f29c5b0
IshanSinha-26 <ishansinha2611@gmail.com>
2026-08-13 16:37:28 +0530

    update palindrome

--- String/palindromeCheck.java
+++ String/palindromeCheck.java
@@ -1,3 +1,32 @@
+// import java.util.*;
+// public class palindromeCheck {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in); 
+
+//         System.out.print("enter the string: ");
+//         String str=sc.nextLine();
+
+//         int left=0;
+//         int right=str.length()-1;
+//         boolean palindrome=true;
+
+//         while(left<right){
+//             if(str.charAt(left)!=str.charAt(right)){
+//                 palindrome=false;
+//                 break;
+//             }
+//             left++;
+//             right--;
+//         }
+//         if(palindrome==true){
+//             System.out.print("this is palindrome");
+//         }else{
+//             System.out.print("this is not palindrome");
+//         }
+//         sc.close();
+//     }
+// }
+
 import java.util.*;
 public class palindromeCheck {
     public static void main(String[] args) {
@@ -6,22 +35,14 @@ public class palindromeCheck {
         System.out.print("enter the string: ");
         String str=sc.nextLine();
 
-        int left=0;
-        int right=str.length()-1;
-        boolean palindrome=true;
-
-        while(left<right){
-            if(str.charAt(left)!=str.charAt(right)){
-                palindrome=false;
-                break;
-            }
-            left++;
-            right--;
+        String reverse="";
+        for(int i=str.length()-1; i>=0; i--){
+            reverse=reverse + str.charAt(i);
         }
-        if(palindrome==true){
-            System.out.print("this is palindrome");
+        if(str.equals(reverse)){
+            System.out.print("it is palindrome");
         }else{
-            System.out.print("this is not palindrome");
+            System.out.print("it is not");
         }
         sc.close();
     }
